00428b10a78d9a1d4e1de19a8f8dcd1117ca2cab
John Choi <johnchoi313@gmail.com>
2021-01-26 23:44:16 -0500

    test 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT Demo.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/ARWT Demo Generic.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
